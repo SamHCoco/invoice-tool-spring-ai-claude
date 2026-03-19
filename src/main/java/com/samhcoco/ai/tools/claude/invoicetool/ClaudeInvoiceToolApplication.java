711350be4210827d640b6994a5ad1fbc0e42fa96
Samuel Coco <Samuel.HenriAC@outlook.com>
2026-03-19 16:43:43 +0000

    Project initialized - setup WIP

---
+++ src/main/java/com/samhcoco/ai/tools/claude/invoicetool/ClaudeInvoiceToolApplication.java
@@ -0,0 +1,13 @@
+package com.samhcoco.ai.tools.claude.invoicetool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaudeInvoiceToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClaudeInvoiceToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samhcoco/ai/tools/claude/invoicetool/ClaudeInvoiceToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samhcoco.ai.tools.claude.invoicetool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClaudeInvoiceToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
